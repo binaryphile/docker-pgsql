4166930f5555ec8233ec3b7f65684a1d857874a3
Ted Lilley <tlilley@diditbetter.com>
2013-10-15 21:47:33 -0400

    Fix typos in prep.sh

--- prep.sh
+++ prep.sh
@@ -2,9 +2,9 @@
 
 export PGVERSION=9.3.1
 export PGNAME=postgresql
-export PGARCHIVE=$PGNAME-PGVERSION.tar.gz
+export PGARCHIVE=$PGNAME-$PGVERSION.tar.gz
 export PGURL=http://ftp.postgresql.org/pub/source/v$PGVERSION/$PGARCHIVE
 
 curl -o $PGARCHIVE $PGURL
-tar xvf $PGARCHIVE
+tar zxvf $PGARCHIVE
 
