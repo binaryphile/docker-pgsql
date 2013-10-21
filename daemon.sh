7be68c33d3ea3d9f1b2dea0cfb09cb748142b71b
Ted Lilley <tlilley@diditbetter.com>
2013-10-21 13:40:13 -0400

    Fix variables

--- daemon.sh
+++ daemon.sh
@@ -4,14 +4,14 @@
 
 : ${ROOT=/root}
 : ${IMAGE=$IX_NAME/$REPO_NAME$TAG}
-: ${USER=postgres}
+: ${PGUSER=postgres}
 : ${PGDATA=$ROOT/postgresql}
 : ${PGCONF=$ROOT/postgresql.conf}
 : ${PGBIN=/usr/local/pgsql/bin}
 : ${PGCMD="$PGBIN/postgres -c config_file=$PGCONF"}
 : ${PGPORT=5432}
 : ${SUDO=""} # change to "sudo" if you aren't in docker group
-: ${OPTIONS="-d -p $PGPORT:5432 -u $USER -v $(pwd):$ROOT -e $PGDATA"}
+: ${OPTIONS="-d -w $ROOT -p $PGPORT:5432 -u $PGUSER -v $(pwd):$ROOT -e ROOT=$ROOT -e PGDATA=$PGDATA"}
 
 $SUDO docker run $OPTIONS $IMAGE $PGCMD
 
--- interactive.sh
+++ interactive.sh
@@ -1,18 +1,19 @@
 : ${IX_NAME?"need to set docker index username IX_NAME, see README.md"}
 : ${REPO_NAME?"need to set repo name REPO_NAME, see README.md"}
-: ${PGVERSION?TAG=:$PG_VERSION}
+: ${SU_NAME?"need to set superuser name SU_NAME, see README.md"}
+: ${SU_PASSWORD?"need to set superuser password SU_PASSWORD, see README.md"}
 
 : ${ROOT=/root}
-: ${IMAGE=$IX_NAME/$REPO_NAME$TAG}
-: ${USER=postgres}
+: ${IMAGE=$IX_NAME/$REPO_NAME:$PGVERSION}
+: ${PGUSER=postgres}
 : ${PGNAME=postgresql}
 : ${PGDATA=$ROOT/$PGNAME}
 : ${PGCONF=$ROOT/postgresql.conf}
 : ${PGBIN=/usr/local/pgsql/bin}
-: ${PGCMD="$PGBIN/postgres -c config_file=$PGCONF"}
+: ${CMD=/bin/bash}
 : ${PGPORT=5432}
 : ${SUDO=""} # change to "sudo" if you aren't in docker group
-: ${OPTIONS="-d -p $PGPORT:5432 -u $USER -v $(pwd):$ROOT -e $PGDATA"}
+: ${OPTIONS="-i -t -w $ROOT -p $PGPORT:5432 -u $PGUSER -v $(pwd):$ROOT -e ROOT=$ROOT -e PGDATA=$PGDATA -e PGNAME=$PGNAME -e SU_NAME=$SU_NAME -e SU_PASSWORD=$SU_PASSWORD"}
 
-$SUDO docker run $OPTIONS $IMAGE $PGCMD
+$SUDO docker run $OPTIONS $IMAGE $CMD
 
