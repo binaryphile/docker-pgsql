be353165e0d0dafa7a95eb63efad1a96eead5f0d
Ted Lilley <tlilley@diditbetter.com>
2013-10-24 14:30:03 -0400

    Refactor scripts

--- create-image.sh
+++ create-image.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 : ${PG_VERSION?"Need to set PG_VERSION, see README.md"}
-: ${SU_NAME?"Need to set database superuser SU_NAME, see README.md"}
+: ${SU_USER?"Need to set database superuser SU_USER, see README.md"}
 : ${SU_PASS?"Need to set database superuser SU_PASS, see README.md"}
 
 : ${ROOT=/root}
 : ${DISTRO=ubuntu:precise}
 : ${PGUSER=postgres}
 : ${PGNAME=postgresql}
-: ${OPTIONS="-i -t -v $(pwd):$ROOT -w $ROOT -e ROOT=$ROOT -e PG_VERSION=$PG_VERSION -e PGNAME=$PGNAME -e PGUSER=$PGUSER -e SU_NAME=$SU_NAME -e SU_PASS=$SU_PASS"}
+: ${OPTIONS="-i -t -v $(pwd):$ROOT -w $ROOT -e ROOT=$ROOT -e PG_VERSION=$PG_VERSION -e PGNAME=$PGNAME -e PGUSER=$PGUSER -e SU_USER=$SU_USER -e SU_PASS=$SU_PASS"}
 : ${PGURL=http://ftp.postgresql.org/pub/source/v$PG_VERSION/$PGNAME-$PG_VERSION.tar.gz}
 : ${CMD=$ROOT/install.sh}
 : ${SUDO=""} # change to "sudo" if you aren't in the docker group
--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-: ${SU_NAME?"need to set superuser name SU_NAME, see README.md"}
+: ${SU_USER?"need to set superuser name SU_USER, see README.md"}
 : ${SU_PASS?"need to set superuser password SU_PASS, see README.md"}
 
 : ${ROOT=/root}
@@ -14,7 +14,7 @@
 
 if [ ! -d $PGDIR ]; then
   $PGINIT $PGDIR
-  $PGCMD --single -c config_file=$PGCONF <<< "CREATE USER $SU_NAME WITH SUPERUSER PASSWORD '$SU_PASS';"
+  $PGCMD --single -c config_file=$PGCONF <<< "CREATE USER $SU_USER WITH SUPERUSER PASSWORD '$SU_PASS';"
 fi
 mkdir -p $ROOT/log
 
